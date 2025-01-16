059111f31adafbf5f2cfadd24446406dbead223b
theSaneHatter <trollroy.com@gmail.com>
2025-01-15 18:48:29 -0700

    GOL_clock2() now works, but need trim empty edges

--- tests1.py
+++ tests1.py
@@ -4,39 +4,78 @@ import numpy as np
 import timeit
 
 
-
+# returns none if array is all 0s
 def GOL_clock(GOL_arr):
+    if np.sum(GOL_arr) <= 0:
+        print('\033[32mWorning from GOL_clock(): Entered array are all zeros\033[0m')
+        return None
     rows, colms = GOL_arr.shape
-    board = np.zeros((rows+2,colms+2),dtype=int)
-    board[1:rows+1,1:colms+1] = GOL_arr[:,:]
-    board_save = board.copy()
+    board_save = np.zeros((rows+2,colms+2),dtype=int)
+    board_save[1:rows+1,1:colms+1] = GOL_arr[:,:]
+    
 
     shift_up = np.roll(board_save,1,0)
-    board = board + shift_up
-    
     shift_down = np.roll(board_save,-1,0)
-    board = board + shift_down
-    
     shift_right = np.roll(board_save,1,1)
-    board = board + shift_right
-
     shift_left = np.roll(board_save,-1,1)
-    board = board + shift_left
+
+    shift_RU = np.roll(shift_right,1,1)
+    shift_RD = np.roll(shift_right,1,-1)
+    shift_LU = np.roll(shift_left,1,1)
+    shift_LD = np.roll(shift_left,1,-1)
+    
+    neighbers = shift_up+shift_down+shift_right+shift_left+shift_RU+shift_RD+shift_LU+shift_LD
+
+    rectified_board = neighbers.copy()
+
+    rectified_board = np.where((rectified_board >= 2) & (rectified_board <= 3), 1, 0)
+
 
     
-    
-    board = np.where((board == 2) & (board == 3), 0,board)
+    return rectified_board
 
-    print(board)
+
+def GOL_clock2(GOL_arr):
+    if np.sum(GOL_arr) <= 0:
+        print('\033[32mWorning from GOL_clock(): Entered array are all zeros\033[0m')
+        return None
+    rows, colms = GOL_arr.shape
+    board_save = np.zeros((rows+2,colms+2),dtype=int)
+    board_save[1:rows+1,1:colms+1] = GOL_arr[:,:]
+    
+
+    shift_up = np.roll(board_save,1,0)
+    shift_down = np.roll(board_save,-1,0)
+    shift_right = np.roll(board_save,1,1)
+    shift_left = np.roll(board_save,-1,1)
+
+    shift_RU = np.roll(shift_right,1,0)
+    shift_RD = np.roll(shift_right,-1,0)
+    shift_LU = np.roll(shift_left,1,0)
+    shift_LD = np.roll(shift_left,-1,0)
+    
+    neighbers = shift_up+shift_down+shift_right+shift_left+shift_RU+shift_RD+shift_LU+shift_LD
+
+    rectified_board = np.where((board_save == 1) & ((neighbers < 2) | (neighbers > 3)), 0, board_save)
+    rectified_board = np.where((board_save == 0) & (neighbers == 3), 1, rectified_board)
+    
+    return rectified_board
+
 
 arr = np.random.randint(0,2,(5,5))
 board = [
     
     [0,1,0],
     [0,1,0],
-    [0,1,0]
+    [0,1,0],
+    [1,1,1]
 ]
 board = np.array(board)
 
 print('Original Arr\n',board)
-GOL_clock(board)
+print('GOL_CLOCK2(board):\n',GOL_clock2(board))
+
+while True:
+    board = GOL_clock2(board)
+    print('GOL_CLOCK2(board):\n',GOL_clock2(board))
+    time.sleep(1)
